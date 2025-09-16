05e69b7777d9c2d8cee93018dc446863f40d505c
정유리 <2024184029@tukorea.ac.kr>
2025-09-16 22:43:21 +0900

    Drill #3 제출 - 2024184029 정유리

--- character_moves.py
+++ character_moves.py
@@ -6,9 +6,6 @@ open_canvas()
 boy = load_image('character.png')
 grass = load_image('grass.png')
 
-# 캐릭터 사각 운동, 삼각 운동, 원운동을 무한 반복
-# 점진적 개발에 따른 커밋이 이루어지면서 개발되고, 모든 기능을 만족하면 3점
-
 def move_top():
     print('Move top')
     for x in range(770, 30 - 1, -5):
